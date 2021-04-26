6d1ace32211f6b642fbfc3a6da5ac2c74d17b41d
Xiaoyi Shen <36715733+Xiaoyis2@users.noreply.github.com>
2021-04-26 00:24:54 -0400

    front end done

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <header className="Landing-page">
+        Welcome to the Chat App!
+      </header>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <div style={ChatAppStyle}>Chat App</div> */}
+        <div style={{ height: 100, color: 'aqua'}}>
+        CHAT APP
+        </div>          
+        <input class="input" type="text" placeholder="User Name"></input>
+        <input class="input" type="text" placeholder="Password"></input>
+
+        <a class="button is-primary" href="https://reactjs.org" >Log In</a> 
+      {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Log In
+        </a> */}
       </header>
     </div>
   );
 }
+
 
 export default App;
